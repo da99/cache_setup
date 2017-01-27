d4a247da77a3ff351a720c569d50d3ae4987e82a
da99 <i-hate-spam-1234567@mailinator.com>
2017-01-27 17:34:01 -0600

    Fixes and touchups.

--- bin/public/get/_.sh
+++ bin/public/get/_.sh
@@ -1,25 +1,25 @@
 
-# === {{CMD}} file key
+# === {{CMD}} key file
 get () {
-  local +x FILE="$1"; shift
   local +x KEY="$1"; shift
+  local +x FILE="$1"; shift
 
   if [[ ! -e "$FILE" ]]; then
     echo "!!! File does not exist: $FILE" >&2
     exit 1
   fi
 
-  local +x VAL="$(awk '/^'$KEY': +([^\n]+)/{print $2}' "$FILE")"
+  local +x VAL="$(sed -n -e 's/'$KEY':\ \+\(.\+\)/\1/p' "$FILE")"
   if [[ ! -z "$VAL" ]]; then
     echo $VAL
     return 0
   fi
 
-  local +x KEY_FOUND="$(awk -F':' '/^('$KEY'):/{print $1}' "$FILE")"
+  local +x KEY_FOUND="$(awk -F':' '/^'$KEY':/{print $1}' "$FILE")"
   if [[ "$KEY_FOUND" == "$KEY" ]]; then
     return 0
   fi
 
-  echo "!!! Key not found: $KEY $@"
+  echo "!!! Key not found: $KEY $@" >&2
   exit 1
 } # === end function
